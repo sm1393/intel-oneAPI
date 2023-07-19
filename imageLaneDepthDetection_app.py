076af87cf49bebae0852f16983610760925afb74
sm1393 <swapnilmane1393@gmail.com>
2023-07-19 23:00:11 +0530

    Final submission after hackthon

--- imageLaneDepthDetection_app.py
+++ imageLaneDepthDetection_app.py
@@ -7,32 +7,43 @@ import numpy as np
 from ultrafastLaneDetector import UltrafastLaneDetector, ModelType
 from MidasDepthEstimation.midasDepthEstimator import midasDepthEstimator
 import matplotlib.pyplot as plt
+import intel_extension_for_pytorch as ipex
+import torch.optim as optim
 
 class_names = ['train','hot dog','skis','snowboard', 'sports ball','baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 'pizza', 'donut', 'cake','teddy bear', 'hair drier', 'toothbrush']  # Add more class names as per your requirement to remove
 
 @st.cache_resource
 def loadModel():
-    model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+    global model, lane_detector, depthEstimator
     model_path = "models/tusimple_18.pth"
     model_type = ModelType.TUSIMPLE
     use_gpu = False
+    model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
     lane_detector = UltrafastLaneDetector(model_path, model_type, use_gpu)
     depthEstimator = midasDepthEstimator()
     return model, lane_detector, depthEstimator
 
 model, lane_detector, depthEstimator = loadModel()
 
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+model = ipex.optimize(model, optimizer=optimizer, dtype=torch.float)
+lane_detector = ipex.optimize(lane_detector, optimizer=optimizer, dtype=torch.float)
+depthEstimator = ipex.optimize(depthEstimator, optimizer=optimizer, dtype=torch.float)
+
 def makeInference(_image):
+    global model, lane_detector, depthEstimator
     st.image(_image, caption='Image')
     image = np.array(_image)
-    output_img = lane_detector.detect_lanes(image)
+    with torch.no_grad():
+        output_img = lane_detector.detect_lanes(image)
+        colorDepth = depthEstimator.estimateDepth(image)
+        results = model(image)
 
-    colorDepth = depthEstimator.estimateDepth(image)
     combinedImg = cv2.addWeighted(image, 0.7,colorDepth,0.6,0)
     img_out = np.hstack((image, colorDepth, combinedImg))
 
     image = cv2.cvtColor(output_img , cv2.COLOR_BGR2RGB)
-    results = model(image)
     boxes = results.pandas().xyxy[0]
     class_labels = results.pandas().xyxy[0]['name']
     confidences = results.pandas().xyxy[0]['confidence']
